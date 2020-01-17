321347ec7e2527443edba3bf305b8d4daf00081b
FranciscoUriel <59972929+FranciscoUriel@users.noreply.github.com>
2020-01-17 13:21:59 -0700

    Agregado de la funcion de borrado de productos

---
+++ Tienda/Controllers/ProductosController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Tienda.Persistence.DBContext;
+
+namespace Tienda.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Productos")]
+    public class ProductosController : ControllerBase
+    {
+        private readonly TiendaContext _contexto;
+        public ProductosController(TiendaContext context)
+        {
+            _contexto = context;
+        }
+
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var producto = await _contexto.Producto.FindAsync(id);
+            if(producto == null)
+            {
+                return NoContent();
+            }
+            _contexto.Remove(producto);
+
+            await _contexto.SaveChangesAsync();
+            
+            return Ok();
+        }
+
+
+    }
+}
--- Tienda/Pages/Index.cshtml.cs
+++ Tienda/Pages/Index.cshtml.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Tienda.Persistence.Entities;
 
 namespace Tienda
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly Tienda.Persistence.DBContext.TiendaContext _context;
 
+        public IndexModel(Tienda.Persistence.DBContext.TiendaContext context)
+        {
+            _context = context;
+        }
+        public IList<Producto> productos { get; set; }
+        public IList<Categoria> categorias { get; set; }
+        public async Task OnGetAsync()
+        {
+            productos = await _context.Producto.ToListAsync();
+            categorias = await _context.Categroria.ToListAsync();
         }
     }
 }
